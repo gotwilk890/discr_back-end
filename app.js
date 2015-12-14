441fc92dbbce87e803931cc4e9ff3cf3babd30e4
gotwilk890 <dave.wilk890@gmail.com>
2015-12-14 10:35:41 -0500

    Adds sessions and connects routes to app.js

--- app.js
+++ app.js
@@ -4,6 +4,14 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var session = require('express-session');
+var uuid = require('uuid');
+var MongoStore = require('connect-mongo')(session);
+var cors = require('cors');
+
+process.env.SESSION_SECRET || require('dotenv').load();
+
+var passport = require('./lib/passport');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -21,9 +29,31 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+  secret : process.env.SESSION_SECRET,
+  resave : false,
+  saveUninitialized : false,
+  store : new MongoStore({
+    url : "mongodb://localhost/discrdb-sessions"
+  }),
+  cookie : {
+    maxAge : 1800000 // 30 minutes
+  },
+  genid : function() {
+    return uuid.v4({
+      rng : uuid.nodeRNG
+    });
+  }
+}));
+
+app.use(passport.initialize());
+
+app.use(passport.session());
 
 app.use('/', routes);
 app.use('/users', users);
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
